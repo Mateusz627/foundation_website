1fe306d5b61eeeb1aefd322f5c36d864790ea618
Mateusz Siuda <131684931+Mateusz627@users.noreply.github.com>
2023-08-04 15:30:26 +0200

    completed home three columns view

--- src/components/HomeThreeColumns.jsx
+++ src/components/HomeThreeColumns.jsx
@@ -1,6 +1,27 @@
+import '../scss/HomeThreeColumns.scss'
 function HomeThreeColumns() {
     return (
-        <div></div>
+        <>
+
+            <div className='main-section'>
+                <div className='data-item'>
+                    <h2 className='data-item__number'>10</h2>
+                    <h3 className='data-item__title'>ODDANYCH WORKÓW</h3>
+                    <p className='data-item__description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <div className='data-item'>
+                    <h2 className='data-item__number'>5</h2>
+                    <h3 className='data-item__title'>WSPARTYCH ORGANIZACJI</h3>
+                    <p className='data-item__description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <div className='data-item'>
+                    <h2 className='data-item__number'>7</h2>
+                    <h3 className='data-item__title'>ZORGANIZOWANYCH ZBIÓREK</h3>
+                    <p className='data-item__description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+
+        </>
     );
 }
 
