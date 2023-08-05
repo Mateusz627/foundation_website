aeca18de1f04d26ccf7806c09efb305d89b104db
Mateusz Siuda <131684931+Mateusz627@users.noreply.github.com>
2023-08-05 16:17:21 +0200

    make home simple steps view

--- src/components/HomeMainSection.jsx
+++ src/components/HomeMainSection.jsx
@@ -1,21 +1,17 @@
-import '../scss/HomeMainSection.scss'
 import Navigation from "./Navigation.jsx";
+import '../scss/HomeMainSection.scss';
+
 function HomeMainSection() {
     return (
         <>
-
             <div className='home-main-section'>
-
-                <div className="home-img"></div>
-
+                <div className="home-img" />
                 <div className='home-content'>
                     <Navigation />
-
                     <div className='home-text'>
                             <h2>Zacznij Pomagać! </h2>
                             <h2>Oddaj niechciane rzeczy w zaufane ręce</h2>
                             <img src="src/assets/Decoration.svg" alt="decoration"/>
-
                         <div className="home-buttons">
                             <button className="home-btn">
                                  <p>ODDAJ</p>
