dd77671ad7af422d122ae746cbdfd2b2700911b7
Mateusz Siuda <131684931+Mateusz627@users.noreply.github.com>
2023-08-07 14:18:23 +0200

    finished view of the simple steps in the house

--- src/components/HomeSimpleSteps.jsx
+++ src/components/HomeSimpleSteps.jsx
@@ -1,40 +1,53 @@
 import '../scss/HomeSimpleSteps.scss'
+import icon1 from '../assets/Icon-1.svg';
+import icon2 from '../assets/Icon-2.svg';
+import icon3 from '../assets/Icon-3.svg';
+import icon4 from '../assets/Icon-4.svg';
+import decoration from '../assets/Decoration.svg'
 function HomeSimpleSteps() {
     return (
         <>
             <div className="step-container">
-                <div className="step">
+                <div className="step-title">
                     <h2>Wystarczą 4 proste kroki</h2>
-                    {/*<image src={decorator} />*/}
-                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="253" height="33" viewBox="0 0 253 33"><defs><clipPath id="a"><rect width="253" height="33" fill="none"/></clipPath></defs><g clip-path="url(#a)"><path d="M160.967,106.2l8.965-8.732-8.965-8.732-8.955,8.732,8.965,8.732" transform="translate(-34.472 -78.774)" fill="none" stroke="#1a1818" stroke-linecap="round" stroke-miterlimit="10" stroke-width="1.788"/><path d="M160.967,97.469l8.965-8.732-8.965-8.732-8.955,8.732Z" transform="translate(-34.472 -78.757)" fill="none" stroke="#1a1818" stroke-linecap="round" stroke-miterlimit="10" stroke-width="1.788"/><line x1="98.316" transform="translate(0.906 17.154)" fill="none" stroke="#1a1818" stroke-linecap="round" stroke-miterlimit="10" stroke-width="1.811"/><path d="M188.708,95.919l-5.638,5.491L172.016,90.645l2.155-2.1,4.679,4.558-4.679,4.558-13.21-12.866-13.19,12.866L143.093,93.1l4.679-4.558,2.155,2.1-11.053,10.766-5.638-5.491" transform="translate(-34.472 -78.757)" fill="none" stroke="#1a1818" stroke-linecap="round" stroke-miterlimit="10" stroke-width="1.788"/><line x1="98.316" transform="translate(153.778 17.154)" fill="none" stroke="#1a1818" stroke-linecap="round" stroke-miterlimit="10" stroke-width="1.811"/><rect width="7.344" height="7.153" stroke-width="1.788" fill="none" stroke="#1a1818" stroke-linecap="round" stroke-miterlimit="10" transform="translate(121.307 26.678) rotate(-45)"/><path d="M192.834,100.733h-4.3l-5.428-5.287-9.673,9.422-12.464-12.14-12.464,12.14-9.673-9.422-5.428,5.287h-4.3" transform="translate(-34.472 -78.768)" fill="none" stroke="#1a1818" stroke-linecap="round" stroke-miterlimit="10" stroke-width="1.788"/></g></svg>
+                    <img src={decoration} alt="decoration"/>
                 </div>
                 <div className="step-item">
                     <div className="step-content">
-                        img
+                        <img src={icon1} alt="clothing"/>
                         <h2>Wybierz rzeczy</h2>
-                        line
-                        <p>ubrania, zabawki, sprzęt i inne</p>
+                        <div className="step-line"></div>
+                        <p>ubrania, zabawki,</p>
+                        <p>sprzęt i inne</p>
                     </div>
                     <div className="step-content">
-                        img
+                        <img src={icon2} alt="shopping cart"/>
                         <h2>Spakuj je</h2>
-                        line
-                        <p>skorzystaj z worków na śmieci</p>
+                        <div className="step-line"></div>
+                        <p>skorzystaj z</p>
+                        <p>worków na śmieci</p>
                     </div>
                     <div className="step-content">
-                        img
-                        <h2>Zdecyduj komu chcesz pomóc</h2>
-                        line
-                        <p>wybierz zaufane miejsce</p>
+                        <img src={icon3} alt="loupe"/>
+                        <div className="step-title">
+                            <h2>Zdecyduj komu</h2>
+                            <h2>chcesz pomóc</h2>
+                        </div>
+                        <div className="step-line"></div>
+                        <div className="step-text">
+                            <p>wybierz zaufane</p>
+                            <p>miejsce</p>
+                        </div>
                     </div>
                     <div className="step-content">
-                        img
+                        <img src={icon4} alt="refreshment"/>
                         <h2>Zamów kuriera</h2>
-                        line
-                        <p>kurier przyjedzie w dogodnym terminie</p>
+                        <div className="step-line"></div>
+                        <p>kurier przyjedzie</p>
+                        <p>w dogodnym terminie</p>
                     </div>
                 </div>
-                <button>ODDAJ RZECZY</button>
+                <button className="step-button">ODDAJ RZECZY</button>
             </div>
         </>
     );
